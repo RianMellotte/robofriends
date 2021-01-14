b721780342605bfc459459a74c17d3032a114f38
Rian Mellotte <Alfred@MacBook-Pro-2.local>
2021-01-14 21:12:54 +0000

    converting app to use hooks

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import CardList from '../components/CardList';
 import Searchbox from '../components/Searchbox';
 import Scroll from '../components/Scroll';
@@ -6,44 +6,36 @@ import ErrorBoundary from '../components/ErrorBoundary';
 import './App.css'
  
 
-class App extends Component {
-	constructor() {
-		super()
-		this.state = {
-			robots: [],
-			searchfield: ''
-		}
-	}
-	
-	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-		.then(response => response.json())
-		.then(users => this.setState({robots: users}))
-	}
-
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value}) 
-	}
-
-	render() {
-		const { robots, searchfield } = this.state
-		const filteredRobots = robots.filter(robot => {
+function App() {
+	const [ robots,	setRobots ] = useState([]);
+	const [ searchfield, setSearchfield ] = useState('');
+	const filteredRobots = robots.filter(robot => {
 			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		})
-		return !robots.length ?
+	
+	useEffect(() => {
+		fetch('https://jsonplaceholder.typicode.com/users')
+		.then(response => response.json())
+		.then(users => setRobots(users))
+	});
+
+	const onSearchChange = (event) => {
+		setSearchfield(event.target.value);
+	}
+
+	return ( !robots.length ?
 		<h1>Loading</h1> :
 		(
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
-				<Searchbox searchChange={this.onSearchChange}/>
+				<Searchbox searchChange={onSearchChange}/>
 				<Scroll>
 					<ErrorBoundary>						
 						<CardList robots={ filteredRobots }/>
 					</ErrorBoundary>
 				</Scroll>
 			</div>
-		);
-	}
-}
+		)
+)}
 
 export default App
